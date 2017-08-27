efa7a724426b81e72ee9d132bcea98d08cb87828
AnaBoshkovska <anaboshkovska.ab@gmail.com>
2017-08-28 01:10:05 +0200

    Added connection to MongoDB and service for getting sensors.

---
+++ public/server/models/Map.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var mapSchema = new Schema({
+    red: Number,
+    green: Number,
+    orange: Number,
+    brown: Number,
+    sensor_id: Number,
+    created_at: Date
+});
+
+var Map = mongoose.model('Map', mapSchema);
+exports.Map = Map;
--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/client"));
 app.listen(3000);
 console.log('Server started on port 3000');
 
 var options = {
-    phantomPath: "C:\\PhantomJs\\bin\\phantomjs\\bin\\phantomjs.exe",
-    siteType: 'html'
+
 };
 
 var webshot = require('webshot');
 webshot('google.com', 'google.png', options, (err) => {
-    // screenshot now saved to google.png
+    // screenshot now saved to google.pnghttps://www.google.mk/maps/@42.0144456,21.3966958,13z/data=!5m1!1e1?hl=en
 });
+
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/climate');
+
+
+var mapModel = require('./public/server/models/Map');
+var Map = mapModel.Map;
+
+
+var map = new Map({
+    red: 3,
+    green: 2,
+    orange: 18,
+    brown: 30,
+    sensor_id: 2
+});
+
+map.save(function(err){
+   if(err) throw err;
+
+    console.log("Map saved");
+});
+
+Map.find({}, function(err, maps){
+    if(err) throw err;
+
+    console.log(maps);
+});
+
+
+var sensorService = require('./public/server/services/SensorService');
+
+sensorService.getSensors();
